b8d73cf9235b26663f4a7c2db7b26ce43f1ea089
Дмитрий Гурьянов <givy11@gmail.com>
2018-07-12 01:33:29 +0300

    Переключение Bootstrap на локальную версию

--- index.php
+++ index.php
@@ -9,9 +9,8 @@ print "";
 	<head>
 		<title><?=$configuration['System']['system_name'] ?> </title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link rel="stylesheet" href="style/css/workplace.css" type="text/css" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-              integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="style/libs/bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="style/css/workplace.css" type="text/css">
 	</head>
 
 	<body id="workplace">
@@ -23,9 +22,7 @@ print "";
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
                 integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
                 crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
-                integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-                crossorigin="anonymous"></script>
+        <script src="style/libs/bootstrap/js/bootstrap.js"></script>
 	</body>
 </html>
 
--- login.php
+++ login.php
@@ -14,8 +14,7 @@ if ($_POST['username'] && $_POST['password']){
     <title><?=$configuration['System']['system_name'] ?> | <?=$configuration['Translation']['login']['authorisation'] ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link href="style/css/login.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-          integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/libs/bootstrap/css/bootstrap.css">
 
 </head>
 
@@ -71,19 +70,11 @@ if ($_POST['username'] && $_POST['password']){
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
         integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
         crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
-        integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-        crossorigin="anonymous"></script>
+<script src="style/libs/bootstrap/js/bootstrap.js"></script>
 
 <script>
     $('#loginModal').modal({backdrop: 'static'});
-    <?
-    if ($_GET['error'] == 1) :
-        print 'alert(1324);
-                document.getElementById("log").value="' . $_GET['login'] . '";';
-    endif;
-    ?></script>
-
+</script>
 
 </body>
 </html>
