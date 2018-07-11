b742b9a2f1513686ccb0f53d8d5bf273a6a8868c
Дмитрий Гурьянов <givy11@gmail.com>
2018-07-12 01:14:07 +0300

    Рефакторинг структуры файлов в связи с изменением архитектуры

--- users/admin/applications/bountyhunter/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-print "<div>Welcome to the party!<br>";
-print "<form action=kill.php>";
-print "&alpha; &nbsp; = <input type=text size=6 name='alpha' value='35'>degrees<br>V<sub>0</sub> = <input type=text size=6 name='V' value='18'>mps<br><input type=submit value=start></div>";
-?>
--- users/admin/applications/icosave/saveico.php
+++
@@ -1,7 +0,0 @@
-<?
-session_start();
-//error_reporting ( E_ERROR | E_PARSE );
-$db=sqlite_open('../../system.db');
-sqlite_query($db, "UPDATE icons SET x='".$_GET['x']."',y='".$_GET['y']."' WHERE layer='".$_GET['layer']."';"); 
-
-?>
